3599c2a32ac3d62e89f08fcafd98ac79ecbb0117
Andrew156697 <andrew@chtlab.io>
2024-12-11 17:32:30 +0700

    asf

--- python_addon/README.md
+++ python_addon/README.md
@@ -4,11 +4,5 @@
 Add-on này sử dụng OpenCV để capture video từ webcam và hiển thị nó.
 
 ## Cách cài đặt
-1. **Tạo Dockerfile** theo mẫu dưới đây và thay đổi `your_custom_repo` thành nơi chứa Docker image của bạn:
-   ```Dockerfile
-   FROM python:3.9-slim
-
-   RUN apt-get update && apt-get install -y ffmpeg libopencv-dev && apt-get clean && rm -rf /var/lib/apt/lists/*
-   WORKDIR /app
-   COPY run.py /run.py
-   CMD ["python", "run.py"]
+1. **Tạo Dockerfile** theo mẫu dưới đây và thay đổi `` thành nơi chứa Docker image của bạn:
+ 
--- python_addon/run.py
+++ python_addon/run.py
@@ -1,1 +1,24 @@
-print("Hello world")
+import cv2
+import logging
+from time import sleep
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+camera = cv2.VideoCapture(0)
+while True:
+    success,frame = camera.read()
+    if not success:
+        print("Không thể lấy frame từ camera.")
+        break
+    else: 
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(rgb_frame)
+        if results.pose_landmarks:
+            for idx, landmark in enumerate(results.pose_landmarks.landmark):
+                x = landmark.x  # Normalized x-coordinate
+                y = landmark.y  # Normalized y-coordinate
+                z = landmark.z  # Depth
+                print(f"Landmark {idx}: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+        sleep(1)
