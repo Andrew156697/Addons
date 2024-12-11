7c22dc4b848a6234dbf4b067c4cb2f053d4ba169
Andrew156697 <andrew@chtlab.io>
2024-12-11 13:48:24 +0700

    assf

--- python_addon/run.py
+++ python_addon/run.py
@@ -1,7 +1,24 @@
+import cv2
+import logging
 from time import sleep
 
-count = 0 
-while(1):
-    print(count)
-    count = count + 1
+# Cấu hình logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+count = 0
+urL_video = "./test.mp4"
+# Mở webcam
+cap = cv2.VideoCapture(0)  # 0 cho camera đầu tiên
+
+while True:
+    # Lấy từng frame
+    ret, frame = cap.read()
+    count += 1
+    if not ret:
+        logging.error("Không thể lấy frame từ camera.")
+        print("Không thể lấy frame từ camera.")
+        break
+    # Hiển thị thông tin trong log
+    print(ret)
+    logging.info(f"Frame count: {count}, Camera read success: {ret}")
     sleep(1)
