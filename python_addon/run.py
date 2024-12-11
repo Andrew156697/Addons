204c91df7ad9f568a6c1752f2462a49d8f31fb91
Andrew156697 <andrew@chtlab.io>
2024-12-12 00:43:50 +0700

    test

--- python_addon/run.py
+++ python_addon/run.py
@@ -6,8 +6,8 @@ import mediapipe as mp
 
 os.environ["TF_DELEGATE_OPTIONS"] = "0"
 
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
+# mp_pose = mp.solutions.pose
+# pose = mp_pose.Pose()
 
 camera = cv2.VideoCapture(0)
 while True:
@@ -16,12 +16,13 @@ while True:
         print("Không thể lấy frame từ camera.")
         break
     else: 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(rgb_frame)
-        if results.pose_landmarks:
-            for idx, landmark in enumerate(results.pose_landmarks.landmark):
-                x, y, z = landmark.x, landmark.y, landmark.z  # Normalized coordinates
-                print(f"Landmark {idx}: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # results = pose.process(rgb_frame)
+        # if results.pose_landmarks:
+        #     for idx, landmark in enumerate(results.pose_landmarks.landmark):
+        #         x, y, z = landmark.x, landmark.y, landmark.z  # Normalized coordinates
+        #         print(f"Landmark {idx}: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+        print("Connected!")
 
         sleep(1)
 camera.release()
