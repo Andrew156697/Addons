bd2f90f000a4ea385a2b4673338b7e49b5e50182
Andrew156697 <andrew@chtlab.io>
2024-12-11 13:33:29 +0700

    asdasd

--- mediapose/run.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-
-count = 0
-while(1):
-    print(count)
-    count = count + 1
-    sleep(1)
--- python_addon/run.py
+++ python_addon/run.py
@@ -1,23 +1,7 @@
-import cv2
-import logging
 from time import sleep
 
-# Cấu hình logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-count = 0
-# Mở webcam
-cap = cv2.VideoCapture(0)  # 0 cho camera đầu tiên
-
-while True:
-    # Lấy từng frame
-    ret, frame = cap.read()
-    count += 1
-    if not ret:
-        logging.error("Không thể lấy frame từ camera.")
-        print("Không thể lấy frame từ camera.")
-        break
-    # Hiển thị thông tin trong log
-    print(ret)
-    logging.info(f"Frame count: {count}, Camera read success: {ret}")
+count = 0 
+while(1):
+    print(count)
+    count = count + 1
     sleep(1)
