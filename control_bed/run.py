95ad0cb606016b37714b7dbad16f6fc9959e02f5
Andrew156697 <andrew@chtlab.io>
2025-01-08 10:32:25 +0700

    send data rs485

--- control_bed/run.py
+++ control_bed/run.py
@@ -2,18 +2,23 @@ import serial
 import time
 import logging
 
+response = ""
+# Forward
+# prefix: # | first=0/1 | pause= 0/1 | head = 0->100| foot = 0->100| lean = 0->100|  
+forward_frame = "#00505050"
+# state_send = False
 # Cấu hình logging
 logging.basicConfig(
-    level=logging.INFO,  # Thay đổi thành DEBUG để xem nhiều thông tin hơn
+    level=logging.INFO,  
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 
 # Cấu hình cổng UART
-SERIAL_PORT = '/dev/ttyAMA0'  # Cổng UART0 trên Orange Pi
+SERIAL_PORT = '/dev/ttyAMA0'  
 BAUDRATE = 9600
-
 def send_and_wait(ser, command, expected_response, timeout=5):
+    global response
     while True:
         # Gửi lệnh qua RS485
         ser.write(command.encode('utf-8'))
@@ -29,27 +34,19 @@ def send_and_wait(ser, command, expected_response, timeout=5):
                     return True
         logging.warning("No valid response, resending...")  # Nếu không nhận được phản hồi đúng, gửi lại lệnh
 
+
+# current state --> received from
+
+
 try:
     # Mở cổng serial
     ser = serial.Serial(SERIAL_PORT, baudrate=BAUDRATE, timeout=1)
     logging.info(f"Connected to {SERIAL_PORT} at {BAUDRATE} baudrate.")
 
     while True:
-        # Bật LED ESP1
-        send_and_wait(ser, "1:LED_ON\n", "1:ACK_LED_ON")
+        send_and_wait(ser, forward_frame, "1:ACK_LED_ON")
         time.sleep(0.5)
 
-        # Bật LED ESP2
-        send_and_wait(ser, "2:LED_ON\n", "2:ACK_LED_ON")
-        time.sleep(0.5)
-
-        # Tắt LED ESP1
-        send_and_wait(ser, "1:LED_OFF\n", "1:ACK_LED_OFF")
-        time.sleep(0.5)
-
-        # Tắt LED ESP2
-        send_and_wait(ser, "2:LED_OFF\n", "2:ACK_LED_OFF")
-        time.sleep(0.5)
 
 except serial.SerialException as e:
     logging.error(f"Serial error: {e}")
